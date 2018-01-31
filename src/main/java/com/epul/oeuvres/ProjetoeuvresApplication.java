67ca5f8965be09ef5c4a457e30862b1049a65535
Clément Serrano <clementserrano3@gmail.com>
2018-01-31 11:39:08 +0100

    first commit

---
+++ src/main/java/com/epul/oeuvres/ProjetoeuvresApplication.java
@@ -0,0 +1,12 @@
+package com.epul.oeuvres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoeuvresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoeuvresApplication.class, args);
+	}
+}
